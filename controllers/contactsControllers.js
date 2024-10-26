1fecfe7124b45b6b033f36c86a7ba7c4c9f20796
ImaLorikI <120187054+ImaLorikI@users.noreply.github.com>
2024-10-26 18:56:37 +0300

    Initialize 03-mongodb

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,87 @@
+
+import { Contacts } from "../models/model.js";
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const getUsers = await Contacts.find();
+    res.status(200).json(getUsers);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const getOne = await Contacts.findById(id);
+    if (!getOne) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(getOne).status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Not found" });
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  try {
+    const deleteContact = await Contacts.findByIdAndDelete(req.params.id);
+    if (!deleteContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(deleteContact).status(200);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createContact = async (req, res) => {
+  try {
+    const newUser = await Contacts.create(req.body);
+    if (!newUser) {
+      return res.status(400).json({ message: "Not found" });
+    }
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Bad request" });
+  }
+};
+
+export const updateContact = async (req, res) => {
+try {
+  const update = await Contacts.findByIdAndUpdate(req.params.id, req.body, { new: true });
+   if (Object.keys(req.body).length === 0) {
+     return res
+       .status(400)
+       .json({ message: "Body must have at least one field" });
+   }
+  if (!update) {
+    return res.status(400).json({ message: "Not found" });
+  }
+  res.json(update).status(200);
+} catch (error) {
+  console.log(error);
+  res.status(404).json({ message: "Not found" });
+ 
+}
+};
+
+
+export const updateStatus = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const update = await Contacts.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!update) {
+      return res.status(400).json({ message: "Not found" });
+    }
+    res.status(200).json(update);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Not found" });
+  }
+};
+
---
+++ services/contactsServices.js
@@ -0,0 +1,73 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+const contactsPath = path.join("db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((id) => id.id === contactId);
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(({ id }) => id === contactId);
+    if (idx === -1) {
+      return null;
+    }
+    const [contact] = contacts.splice(idx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+const addContact = async (body) => {
+  try {
+    const list = await listContacts();
+    const newContact = {
+      ...body,
+    };
+    list.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(list));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateContactById = async (id, body) => {
+  try {
+    const list = await listContacts();
+    const index = list.findIndex((el) => el.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const updatedContact = { ...list[index], ...body };
+    list[index] = updatedContact;
+    await fs.writeFile(contactsPath, JSON.stringify(list));
+    return updatedContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { listContacts, getContactById, removeContact, addContact };
